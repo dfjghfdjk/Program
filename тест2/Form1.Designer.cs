365756cb5de02cc59b0173b7b89feeadf865b0e0
Admin <Dl9regaa@gmail.com>
2024-05-02 23:52:03 +0300

    Add project files.

---
+++ тест2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace тест2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nozhik", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(125, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Ink Free", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTask.Location = new System.Drawing.Point(125, 136);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(569, 37);
+            this.txtTask.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Nozhik", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(536, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Nozhik", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(610, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 103);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.Font = new System.Drawing.Font("Ink Free", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 30;
+            this.lstTasks.Location = new System.Drawing.Point(3, 235);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(601, 214);
+            this.lstTasks.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Nozhik", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(610, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 103);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Редактировать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::тест2.Properties.Resources.Метеор;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ тест2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace тест2
+{
+    public partial class Form1 : Form
+    {
+        private List<string> tasks = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newTask = txtTask.Text.Trim();
+            if (!string.IsNullOrEmpty(newTask))
+            {
+                int selectedIndex = lstTasks.SelectedIndex;
+                if (selectedIndex != -1)
+                {
+                    tasks[selectedIndex] = newTask;
+                    lstTasks.Items[selectedIndex] = newTask;
+                }
+                else
+                {
+                    tasks.Add(newTask);
+                    lstTasks.Items.Add(newTask);
+                }
+                txtTask.Clear();
+                lstTasks.ClearSelected();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                int selectedIndex = lstTasks.SelectedIndex;
+                tasks.RemoveAt(selectedIndex);
+                lstTasks.Items.RemoveAt(selectedIndex);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tasks.Clear();
+            lstTasks.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                string selectedTask = lstTasks.SelectedItem.ToString();
+                txtTask.Text = selectedTask;
+            }
+        }
+    }
+}
